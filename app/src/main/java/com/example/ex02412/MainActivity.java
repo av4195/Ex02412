558bf2b2e70b6581ce44683236cb3c5cb8a7faaf
av4195 <av4195@bs.amalnet.k12.il>
2025-11-08 18:49:20 +0200

    main

--- app/src/main/java/com/example/ex02412/MainActivity.java
+++ app/src/main/java/com/example/ex02412/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ex02412;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +11,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+    }
+
+    public static void main(String[] args) {
+        Guitar guitar1 = new Guitar("brown");
+        Piano piano1 = new Piano("Grand Piano");
+        Drum drum1 = new Drum(3);
+
+        Playble[] playbles = new Playble[3];
+        playbles[0] = guitar1;
+        playbles[1] = piano1;
+        playbles[2] = drum1;
+
+        for (int i = 0; i < 3; i++) {
+            playbles[i].Play();
+        }
     }
 }
